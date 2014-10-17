e08086a5f6dc5153ff579dffd4a1d885146041b7
Russell Keith-Magee <russell@keith-magee.com>
2014-10-17 12:16:29 +0800

    Narrowed module level import range.

--- rubicon/objc/__init__.py
+++ rubicon/objc/__init__.py
@@ -6,5 +6,20 @@ from .objc import objc, send_message, send_super
 from .objc import get_selector
 from .objc import ObjCClass, ObjCInstance, ObjCSubclass
 
-from .core_foundation import *
-from .types import *
+from .core_foundation import at, to_str, to_number, to_value, to_set, to_list
+
+from .types import (
+    text,
+    NSInteger, NSUInteger,
+    CGFloat,
+    NSPointEncoding, NSSizeEncoding, NSRectEncoding, NSRangeEncoding,
+    CGPoint, NSPoint,
+    CGSize, NSSize,
+    CGRect, NSRect,
+    CGSizeMake, NSMakeSize,
+    CGRectMake, NSMakeRect,
+    CGPointMake, NSMakePoint,
+    NSTimeInterval,
+    CFRange, NSRange,
+    NSZeroPoint
+)
