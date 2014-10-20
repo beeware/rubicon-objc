d6548161b7cef9b42b46050759e78e3eb3101884
Russell Keith-Magee <russell@keith-magee.com>
2014-10-20 14:01:25 +0800

    Added protection against the absence of setuptools.

--- rubicon/__init__.py
+++ rubicon/__init__.py
@@ -1,1 +1,8 @@
-__import__("pkg_resources").declare_namespace(__name__)
+from __future__ import print_function
+try:
+    # If we're on iOS, we won't have pkg-resources; but then,
+    # we won't need to register the namespace package, either.
+    # Ignore the error if it occurs.
+    __import__("pkg_resources").declare_namespace(__name__)
+except ImportError:
+    print('Rubicon namespace package not registered!')
