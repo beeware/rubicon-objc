bb2fc22961d16201bf7b366bb01c25051323f6b8
dgelessus <dgelessus@users.noreply.github.com>
2018-05-03 18:35:51 +0200

    Fix sys.path in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 import sys
 import os
-sys.path.insert(0, os.path.abspath('../src/core/'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration -----------------------------------------------------
 
