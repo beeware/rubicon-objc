1c05adcb9cca33296e0c4666f8bff47b7ad6b056
dgelessus <dgelessus@users.noreply.github.com>
2018-05-31 22:46:36 +0200

    Add initial reference docs for rubicon.objc.types

---
+++ docs/reference/rubicon-objc-runtime.rst
@@ -0,0 +1,5 @@
+====================================================================
+:mod:`rubicon.objc.runtime` --- Low-level Objective-C runtime access
+====================================================================
+
+.. module:: rubicon.objc.runtime
--- rubicon/objc/types.py
+++ rubicon/objc/types.py
@@ -91,23 +91,24 @@ _ctype_for_type_map = {
 
 
 def ctype_for_type(tp):
-    """Look up the ctypes type corresponding to the given type.
-    This conversion is applied to types used in objc_method signatures, objc_ivar types, etc.
-    This translates Python built-in types and rubicon.objc classes to their ctypes equivalents.
-    Unregistered types (including things that are already ctypes types) are returned unchanged.
+    """Look up the ctype corresponding to the given Python type.
+
+    This conversion is applied to types used in :class:`objc_method` signatures, :class:`objc_ivar` types, etc.
+    This function translates Python built-in types and :mod:`rubicon.objc` classes to their :mod:`ctypes` equivalents.
+    Unregistered types (including types that are already ctypes) are returned unchanged.
     """
 
     return _ctype_for_type_map.get(tp, tp)
 
 
 def register_ctype_for_type(tp, ctype):
-    """Register a conversion from a type to a ctypes type."""
+    """Register a conversion from a Python type to a ctype."""
 
     _ctype_for_type_map[tp] = ctype
 
 
 def unregister_ctype_for_type(tp):
-    """Unregister a conversion from a type to a ctypes type."""
+    """Unregister a conversion from a Python type to a ctype."""
 
     del _ctype_for_type_map[tp]
 
@@ -292,7 +293,20 @@ def _ctype_for_unknown_encoding(encoding):
 
 
 def ctype_for_encoding(encoding):
-    """Return ctypes type for an encoded Objective-C type."""
+    """Return the ctype corresponding to an Objective-C type encoding.
+
+    If a ctype has been registered for the encoding, that type is returned. Otherwise, if the type encoding represents
+    a compound type (pointer, array, structure, or union), the contained types are converted recursively. A new ctype
+    is then created from the converted ctypes, and is registered for the encoding (so that future conversions of the
+    same encoding return the same ctype).
+
+    For example, the type encoding ``{spam=ic}`` is not registered by default. However, the contained types ``i`` and
+    ``c`` are registered, so they are converted individually and used to create a new :class:`~ctypes.Structure` with
+    two fields of the correct types. The new structure type is then registered for the original encoding ``{spam=ic}``
+    and returned.
+
+    :raises ValueError: if the conversion fails at any point
+    """
 
     # Remove qualifiers, as documented in Table 6-2 here:
     # https://developer.apple.com/library/content/documentation/Cocoa/Conceptual/ObjCRuntimeGuide/Articles/ocrtTypeEncodings.html
@@ -306,7 +320,18 @@ def ctype_for_encoding(encoding):
 
 
 def encoding_for_ctype(ctype):
-    """Return the Objective-C type encoding for the given ctypes type."""
+    """Return the Objective-C type encoding for the given ctypes type.
+
+    If a type encoding has been registered for the ctype, that encoding is returned. Otherwise, if the ctype is a
+    pointer type, its pointed-to type is encoded and used to construct the pointer type encoding.
+
+    Automatic encoding of other compound types (arrays, structures, and unions) is currently not supported. To encode
+    such types, a type encoding must be manually provided for them using :func:`register_preferred_encoding` or
+    :func:`register_encoding`.
+
+    :raises ValueError: if the conversion fails at any point
+    """
+
     try:
         return _encoding_for_ctype_map[ctype]
     except KeyError:
@@ -317,8 +342,10 @@ def encoding_for_ctype(ctype):
 
 
 def register_preferred_encoding(encoding, ctype):
-    """Register a preferred conversion between an Objective-C type encoding and a ctypes type.
-    This overwrites any existing conversions in each direction.
+    """Register a preferred conversion between an Objective-C type encoding and a ctype.
+
+    "Preferred" means that any existing conversions in each direction are overwritten with the new conversion.
+    To register an encoding without overwriting existing conversions, use :func:`register_encoding`.
     """
 
     _ctype_for_encoding_map[encoding] = ctype
@@ -326,8 +353,9 @@ def register_preferred_encoding(encoding, ctype):
 
 
 def with_preferred_encoding(encoding):
-    """Decorator for registering a preferred conversion between the given encoding and the decorated type.
-    This is equivalent to calling register_preferred_encoding.
+    """Register a preferred conversion between an Objective-C type encoding and the decorated ctype.
+
+    This is equivalent to calling :func:`register_preferred_encoding` on the decorated ctype.
     """
 
     def _with_preferred_encoding_decorator(ctype):
@@ -338,8 +366,10 @@ def with_preferred_encoding(encoding):
 
 
 def register_encoding(encoding, ctype):
-    """Register an additional conversion between an Objective-C type encoding and a ctypes type.
-    If a conversion already exists in one or both directions, it is not overwritten.
+    """Register an additional conversion between an Objective-C type encoding and a ctype.
+
+    "Additional" means that any existing conversions in either direction are *not* overwritten with the new conversion.
+    To register an encoding and overwrite existing conversions, use :func:`register_preferred_encoding`.
     """
 
     _ctype_for_encoding_map.setdefault(encoding, ctype)
@@ -347,8 +377,9 @@ def register_encoding(encoding, ctype):
 
 
 def with_encoding(encoding):
-    """Decorator for registering a conversion between the given encoding and the decorated type.
-    This is equivalent to calling register_encoding.
+    """Register an additional conversion between an Objective-C type encoding and the decorated ctype.
+
+    This is equivalent to calling :func:`register_encoding` on the decorated ctype.
     """
 
     def _with_encoding_decorator(ctype):
@@ -359,26 +390,24 @@ def with_encoding(encoding):
 
 
 def unregister_encoding(encoding):
-    """Unregister the conversion between an Objective-C type encoding and its corresponding ctypes type.
+    """Unregister the conversion from an Objective-C type encoding to its corresponding ctype.
 
-    Note that this does not remove any conversions in the other direction
-    (from a ctype to this encoding). These conversions may be replaced with
-    register_encoding, or unregistered with unregister_ctype. To remove all
-    ctypes for an encoding, use unregister_encoding_all.
+    Note that this does not remove any conversions in the other direction (from a ctype to this encoding). These
+    conversions may be replaced with :func:`register_encoding`, or unregistered with :func:`unregister_ctype`.
+    To remove all ctypes for an encoding, use :func:`unregister_encoding_all`.
 
-    If encoding was not registered previously, nothing happens.
+    If the encoding was not registered previously, nothing happens.
     """
 
     _ctype_for_encoding_map.pop(encoding, None)
 
 
 def unregister_encoding_all(encoding):
-    """Unregister all conversions between an Objective-C type encoding and all corresponding ctypes types.
+    """Unregister all conversions between an Objective-C type encoding and all corresponding ctypes.
 
-    All conversions from any ctype to this encoding are removed recursively
-    using unregister_ctype_all.
+    All conversions from any ctype to this encoding are removed recursively using :func:`unregister_ctype_all`.
 
-    If encoding was not registered previously, nothing happens.
+    If the encoding was not registered previously, nothing happens.
     """
 
     _ctype_for_encoding_map.pop(encoding, None)
@@ -388,23 +417,24 @@ def unregister_encoding_all(encoding):
 
 
 def unregister_ctype(ctype):
-    """Unregister the conversion from a ctypes type to an Objective-C type encoding.
+    """Unregister the conversion from a ctype to its corresponding Objective-C type encoding.
 
-    Note that this does not remove any conversions in the other direction
-    (from an encoding to this ctype). These conversions may be replaced with
-    register_encoding, or unregistered with unregister_encoding. To remove all
-    encodings for a ctype, use unregister_ctype_all.
+    Note that this does not remove any conversions in the other direction (from an encoding to this ctype). These
+    conversions may be replaced with :func:`register_encoding`, or unregistered with :func:`unregister_encoding`.
+    To remove all encodings for a ctype, use :func:`unregister_ctype_all`.
 
-    If ctype was not registered previously, nothing happens.
+    If the ctype was not registered previously, nothing happens.
     """
 
     _encoding_for_ctype_map.pop(ctype, default=None)
 
 
 def unregister_ctype_all(ctype):
-    """Unregister all conversions between a ctypes type and all corresponding Objective-C type encodings.
-    All conversions from any type encoding to this ctype are removed recursively using unregister_encoding_all.
-    If ctype was not registered previously, nothing happens.
+    """Unregister all conversions between a ctype and all corresponding Objective-C type encodings.
+
+    All conversions from any type encoding to this ctype are removed recursively using :func:`unregister_encoding_all`.
+
+    If the ctype was not registered previously, nothing happens.
     """
 
     _encoding_for_ctype_map.pop(ctype, default=None)
@@ -449,7 +479,11 @@ def split_method_encoding(encoding):
 
 
 def ctypes_for_method_encoding(encoding):
-    """Convert a method signature encoding into a sequence of ctypes types."""
+    """Convert a method signature encoding into a sequence of ctypes.
+
+    This is equivalent to first splitting the method signature encoding using :func:`split_method_encoding`, and then
+    converting each individual type encoding using :func:`ctype_for_encoding`.
+    """
 
     return [ctype_for_encoding(enc) for enc in split_method_encoding(encoding)]
 
@@ -494,10 +528,22 @@ def _array_for_sequence(seq, array_type):
 
 
 def compound_value_for_sequence(seq, tp):
+    """Create a C structure or array of type ``tp``, initialized with values from ``seq``.
+
+    If ``tp`` is a :class:`~ctypes.Structure` type, the newly created structure's fields are initialized in declaration
+    order with the values from ``seq``. ``seq`` must have as many elements as the structure has fields.
+
+    If ``tp`` is a :class:`~ctypes.Array` type, the newly created array is initialized with the values from ``seq``.
+    ``seq`` must have as many elements as the array type.
+
+    In both cases, if a structure field type or the array element type is itself a structure or array type, the
+    corresponding value from ``seq`` is recursively converted as well.
+    """
+
     if issubclass(tp, Structure):
-        return struct_for_sequence(seq, tp)
+        return _struct_for_sequence(seq, tp)
     elif issubclass(tp, Array):
-        return array_for_sequence(seq, tp)
+        return _array_for_sequence(seq, tp)
     else:
         raise TypeError("Don't know how to convert a sequence to a {tp.__module__}.{tp.__qualname__}".format(tp=tp))
 
@@ -601,14 +647,14 @@ register_preferred_encoding(_PyObjectEncoding, py_object)
 @with_encoding(b'^{?}')
 @with_encoding(b'^(?)')
 class UnknownPointer(c_void_p):
-    """Placeholder for the "unknown pointer" types b'^?', b'^{?}' and b'^(?)'.
+    """Placeholder for the "unknown pointer" types ``^?``, ``^{?}`` and ``^(?)``.
 
-    Not to be confused with a b'^v' void pointer.
+    Not to be confused with a ``^v`` void pointer.
 
-    Usually a b'^?' is a function pointer, but because the encoding doesn't contain the function signature, you need to
+    Usually a ``^?`` is a function pointer, but because the encoding doesn't contain the function signature, you need to
     manually create a CFUNCTYPE with the proper types, and cast this pointer to it.
 
-    b'^{?}' and b'^(?)' are pointers to a structure or union (respectively) with unknown name and fields. Such a type
+    ``^{?}`` and ``^(?)`` are pointers to a structure or union (respectively) with unknown name and fields. Such a type
     also cannot be used meaningfully without casting it to the correct pointer type first.
     """
 
