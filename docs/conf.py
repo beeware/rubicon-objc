c2ceae95cd39daef2ce875e20c9dc0170bb42e02
dgelessus <dgelessus@users.noreply.github.com>
2018-06-14 00:05:05 +0200

    Update docs/conf.py
    Updated copyright year, consolidated imports, fixed Python 3.4/3.5
    compatibility, removed Python 2 compatibility.

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-#
 # Rubicon documentation build configuration file, created by
 # sphinx-quickstart on Sat Jul 27 14:58:42 2013.
 #
@@ -11,11 +9,13 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+import os
+import re
+import sys
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-import sys
-import os
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration -----------------------------------------------------
@@ -40,17 +40,16 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Rubicon'
-copyright = u'2013, Russell Keith-Magee'
+project = 'Rubicon'
+copyright = '2018, Russell Keith-Magee'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-import io, re
-with io.open('../rubicon/objc/__init__.py', encoding='utf8') as version_file:
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
+with open('../rubicon/objc/__init__.py', encoding='utf8') as version_file:
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.MULTILINE)
     if version_match:
         release = version_match.group(1)
     else:
@@ -104,7 +103,7 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:  # only import and set the theme if we're building docs locally
     try:
         import sphinx_rtd_theme
-    except ModuleNotFoundError:
+    except ImportError:
         html_theme = 'default'
     else:
         html_theme = 'sphinx_rtd_theme'
@@ -207,8 +206,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'rubicon.tex', u'Rubicon Documentation',
-   u'Russell Keith-Magee', 'manual'),
+    ('index', 'rubicon.tex', 'Rubicon Documentation', 'Russell Keith-Magee', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -237,8 +235,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'rubicon', u'Rubicon Documentation',
-     [u'Russell Keith-Magee'], 1)
+    ('index', 'rubicon', 'Rubicon Documentation', ['Russell Keith-Magee'], 1),
 ]
 
 # If true, show URL addresses after external links.
@@ -251,9 +248,10 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'rubicon', u'Rubicon Documentation',
-   u'Russell Keith-Magee', 'Rubicon', 'A bridge between an Objective-C runtime environment and Python.',
-   'Miscellaneous'),
+    (
+        'index', 'rubicon', 'Rubicon Documentation', 'Russell Keith-Magee', 'Rubicon',
+        'A bridge between an Objective-C runtime environment and Python.', 'Miscellaneous',
+    ),
 ]
 
 # Documents to append as an appendix to all manuals.
