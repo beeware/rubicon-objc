1e92e32b51650d8e8a44e9b776b2ca3e25933033
dgelessus <dgelessus@users.noreply.github.com>
2020-10-31 18:31:39 +0100

    Replace manual version extraction with setup.cfg attr directive
    This now works, because setuptools 46.4.0 and later extract attribute
    values statically where possible, rather than having to import the
    module in question at runtime. The latter fails in the case of
    rubicon-objc when building on a system that doesn't have a working
    Objective-C runtime.

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-import re
-
 from setuptools import setup
 
-with open('./rubicon/objc/__init__.py', encoding='utf8') as version_file:
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
-    if version_match:
-        version = version_match.group(1)
-    else:
-        raise RuntimeError("Unable to find version string.")
-
-
-setup(
-    version=version,
-)
+setup()
