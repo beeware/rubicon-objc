7c36d99654d00f181572d06c7b8fd96896790875
Sam Schott <ss2151@cam.ac.uk>
2022-02-16 23:43:37 +0100

    add tests for send_super

--- tests/objc/SpecificExample.m
+++ tests/objc/SpecificExample.m
@@ -7,4 +7,21 @@
     self.baseIntField = v + m;
 }
 
+-(void) methodWithArgs: (int) num, ... {
+
+   int prod = 1;
+
+   va_list args;
+   va_start( args, num );
+
+   for( int i = 0; i < num; i++)
+   {
+      prod *= va_arg( args, int);
+   }
+
+   va_end( args );
+
+   self.baseIntField = prod;
+}
+
 @end
