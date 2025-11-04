fc119c197465930a1eec141c82d6192e49f17de6
Kattni <kattni@kattni.com>
2025-11-04 15:44:06 -0500

    Remove duplicate docstrings, update comment.

--- src/rubicon/objc/api.py
+++ src/rubicon/objc/api.py
@@ -1778,230 +1778,19 @@ class ObjCMetaClass(ObjCClass):
 register_ctype_for_type(ObjCInstance, objc_id)
 register_ctype_for_type(ObjCClass, Class)
 
-# check-docstring-is-first made us do this
-if True:
-    NSObject = ObjCClass("NSObject")
-    """
-    The
-    [NSObject](https://developer.apple.com/documentation/objectivec/nsobject?language
-    =objc)
-    class from `<objc/NSObject.h>`.
-
-    See the [`ObjCInstance`][rubicon.objc.api.ObjCInstance]
-    documentation for a list of operations that Rubicon supports on all objects.
-
-    # debugDescription
-
-    ```python
-    debugDescription
-    ```
-
-    Exposes the Objective-C
-    [`debugDescription`](https://developer.apple.com/documentation/objectivec/nsobjectprotocol/debugdescription?language=objc)
-    property.
-
-    # description
-
-    ```python
-    description
-    ```
-
-    Exposes the Objective-C
-    [`description`](https://developer.apple.com/documentation/objectivec/nsobjectprotocol/description?language=objc)
-    property.
-    """
-    NSObject.declare_property("debugDescription")
-    NSObject.declare_property("description")
-    NSNumber = ObjCClass("NSNumber")
-    """
-    The
-    [NSNumber](https://developer.apple.com/documentation/foundation/nsnumber?language=objc)
-    class from `<Foundation/NSValue.h>`.
-
-    This class can be converted to and from standard Python primitives
-    (`bool`, `int`, `float`) using [`py_from_ns`][rubicon.objc.py_from_ns] and
-    [`ns_from_py`][rubicon.objc.ns_from_py].
-    """
-    NSDecimalNumber = ObjCClass("NSDecimalNumber")
-    """
-    The
-    [NSDecimalNumber](https://developer.apple.com/documentation/foundation/nsdecimalnumber?language=objc)
-    class from `<Foundation/NSDecimalNumber.h>`.
-
-    This class can be converted to and from Python `decimal.Decimal` using
-    [`py_from_ns`][rubicon.objc.py_from_ns] and
-    [`ns_from_py`][rubicon.objc.ns_from_py].
-    """
-    NSString = ObjCClass("NSString")
-    r"""
-    The
-    [NSString](https://developer.apple.com/documentation/foundation/nsstring?language=objc)
-    class from `<Foundation/NSString.h>`.
-
-    This class also supports all methods that [`str`][] does.
-
-    This class can be converted to and from Python [`str`][] using
-    [`py_from_ns`][rubicon.objc.py_from_ns] and
-    [`ns_from_py`][rubicon.objc.ns_from_py]. You can also call
-    `str(nsstring)` to convert a `NSString` to [`str`][].
-
-    [`NSString`][rubicon.objc.api.NSString] objects consist of UTF-16
-    code units, unlike [`str`][], which consists
-    of Unicode code points. All [`NSString`][rubicon.objc.api.NSString]
-    indices and iteration are based on UTF-16, even when using the
-    Python-style operations/methods. If indexing or iteration based on code
-    points is required, convert the [`NSString`][rubicon.objc.api.NSString] to
-    [`str`][] first.
-
-    # \_\_str\_\_
-
-    ```python
-    __str__()
-    ```
-
-    Return the value of this [`NSString`][rubicon.objc.api.NSString] as a
-    [`str`][].
-
-    # UTF8String
-
-    ```python
-    UTF8String
-    ```
-
-    This Objective-C property has been declared using
-    [`ObjCClass.declare_property()`][rubicon.objc.api.ObjCClass.declare_property]
-    and can always be accessed using attribute syntax.
-    """
-    NSString.declare_property("UTF8String")
-    NSData = ObjCClass("NSData")
-    """
-    The
-    [NSData](https://developer.apple.com/documentation/foundation/nsdata?language=objc)
-    class from `<Foundation/NSData.h>`.
-
-    This class can be converted to and from Python [`bytes`][] using
-    [`py_from_ns`][rubicon.objc.py_from_ns] and
-    [`ns_from_py`][rubicon.objc.ns_from_py].
-    """
-    NSArray = ObjCClass("NSArray")
-    """
-    The
-    [NSArray](https://developer.apple.com/documentation/foundation/nsarray?language=objc)
-    class from `<Foundation/NSArray.h>`.
-
-    This class can be converted to and from Python [`list`][] using
-    [`py_from_ns`][rubicon.objc.py_from_ns] and
-    [`ns_from_py`][rubicon.objc.ns_from_py].
-
-    `py_from_ns(nsarray)` will recursively convert `nsarray`'s elements to
-    Python objects, where possible. To avoid this recursive conversion, use
-    `list(nsarray)` instead.
-
-    `ns_from_py(pylist)` will recursively convert `pylist`'s elements to
-    Objective-C. As there is no way to store Python object references as
-    Objective-C objects yet, this recursive conversion cannot be avoided. If
-    any of `pylist`'s elements cannot be converted to Objective-C, an error
-    is raised.
-
-    Supports
-    [Python-style sequence operations](https://docs.python.org/3/library/stdtypes.html#typesseq)
-    including: `__getitem__()`, `__len__()`, `__iter__()`, `__contains__()`, `__eq__()`,
-    `__ne__()`, `index()`, `count()`, and `copy()`.
-    """
-    NSMutableArray = ObjCClass("NSMutableArray")
-    """
-    The
-    [NSMutableArray](https://developer.apple.com/documentation/foundation/nsmutablearray?language=objc)
-    class from `<Foundation/NSArray.h>`.
-
-    This class can be converted to and from Python exactly like its
-    superclass `NSArray`.
-
-    Supports
-    [Python-style mutable sequence operations](https://docs.python.org/3/library/stdtypes.html#mutable-sequence-types)
-    including: `__setitem__()`, `__delitem__()`, `append()`, `clear()`, `extend()`,
-    `insert()`, `pop()`, `remove()`, and `reverse()`.
-    """
-    NSDictionary = ObjCClass("NSDictionary")
-    """
-    The
-    [NSDictionary](https://developer.apple.com/documentation/foundation/nsdictionary?language=objc)
-    class from `<Foundation/NSDictionary.h>`.
-
-    This class can be converted to and from Python [`dict`][] using
-    [`py_from_ns`][rubicon.objc.py_from_ns] and
-    [`ns_from_py`][rubicon.objc.ns_from_py].
-
-    `py_from_ns(nsdict)` will recursively convert `nsdict`'s keys and values
-    to Python objects, where possible. To avoid the recursive conversion of
-    the values, use `{py_from_ns(k): v for k, v in nsdict.items()}`. The
-    conversion of the keys cannot be avoided, because Python
-    [`dict`][] keys need to be hashable, which
-    [`ObjCInstance`][rubicon.objc.api.ObjCInstance] is not. If any of the
-    keys convert to a Python object that is not hashable, an error is raised
-    (regardless of which conversion method you use).
-
-    `ns_from_py(pydict)` will recursively convert `pydict`'s keys and values
-    to Objective-C. As there is no way to store Python object references as
-    Objective-C objects yet, this recursive conversion cannot be avoided. If
-    any of `pydict`'s keys or values cannot be converted to Objective-C, an
-    error is raised.
-
-    Supports
-    [Python-style mapping operations](https://docs.python.org/3/library/stdtypes.html#mapping-types-dict)
-    including `__getitem__()`, `__len__()`, `__iter__()`, `__contains__()`, `__eq__()`,
-     `__ne__()`, `copy()`, `get()`, `keys()`, `items()`, and `values()`.
-
-    Unlike most Python mappings, [`NSDictionary`][rubicon.objc.api.NSDictionary]'s
-    [`keys`](https://docs.python.org/3/library/stdtypes.html#dict.keys),
-    [`values`](https://docs.python.org/3/library/stdtypes.html#dict.values), and
-    [`items`](https://docs.python.org/3/library/stdtypes.html#dict.items)
-    methods don't return dynamic views of the dictionary's
-    keys, values, and items.
-
-    [`keys`](https://docs.python.org/3/library/stdtypes.html#dict.keys) and
-    [`values`](https://docs.python.org/3/library/stdtypes.html#dict.values) return
-    lists that are created each time the methods are
-    called, which can have an effect on performance and memory usage for
-    large dictionaries. To avoid this, you can cache the return values of
-    [`keys`](https://docs.python.org/3/library/stdtypes.html#dict.keys) and
-    [`values`](https://docs.python.org/3/library/stdtypes.html#dict.values), or convert
-    the [`NSDictionary`][rubicon.objc.api.NSDictionary] to a Python [`dict`][]
-    beforehand.
-
-    [`items`](https://docs.python.org/3/library/stdtypes.html#dict.items) is currently
-    implemented as a generator, meaning that it returns a single-use iterator. If you
-    need to iterate over
-    [`items`](https://docs.python.org/3/library/stdtypes.html#dict.items) more than
-    once or perform other operations on it, you should convert it to a Python
-    [`set`][] or [`list`][] first.
-    """
-    NSMutableDictionary = ObjCClass("NSMutableDictionary")
-    """
-    The
-    [NSMutableDictionary](https://developer.apple.com/documentation/foundation/nsmutabledictionary?language=objc)
-    class from `<Foundation/NSDictionary.h>`.
-
-    This class can be converted to and from Python exactly like its
-    superclass `NSDictionary`.
-
-    Supports
-    [Python-style mutable mapping operations](https://docs.python.org/3/library/stdtypes.html#mapping-types-dict)
-    including `setitem()`, `delitem()`, `clear()`, `pop()`, `popitem()`, `setdefault()`,
-    and `update()`.
-    """
-    Protocol = ObjCClass("Protocol")
-    """
-    The
-    [Protocol](https://developer.apple.com/documentation/objectivec/protocol?language=objc)
-    class from `<objc/Protocol.h>`.
-
-    This class has no (non-deprecated) Objective-C methods; protocol objects
-    can only be manipulated using Objective-C runtime functions. Rubicon
-    automatically wraps all [`Protocol`][rubicon.objc.api.Protocol]
-    objects using [`ObjCProtocol`][rubicon.objc.api.ObjCProtocol], which
-    provides an easier interface for working with protocols.
-    """
+NSObject = ObjCClass("NSObject")
+NSObject.declare_property("debugDescription")
+NSObject.declare_property("description")
+NSNumber = ObjCClass("NSNumber")
+NSDecimalNumber = ObjCClass("NSDecimalNumber")
+NSString = ObjCClass("NSString")
+NSString.declare_property("UTF8String")
+NSData = ObjCClass("NSData")
+NSArray = ObjCClass("NSArray")
+NSMutableArray = ObjCClass("NSMutableArray")
+NSDictionary = ObjCClass("NSDictionary")
+NSMutableDictionary = ObjCClass("NSMutableDictionary")
+Protocol = ObjCClass("Protocol")
 
 
 def py_from_ns(nsobj):
@@ -2139,10 +1928,7 @@ def ns_from_py(pyobj):
         )
 
 
-# check-docstring-is-first made us do this
-if True:
-    at = ns_from_py
-    """Alias for [`ns_from_py`][rubicon.objc.ns_from_py]."""
+at = ns_from_py
 
 
 @for_objcclass(Protocol)
