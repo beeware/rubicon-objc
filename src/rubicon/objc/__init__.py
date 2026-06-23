fb96955650b19f169c25c9a70c4b93242bb9c735
Russell Keith-Magee <russell@keith-magee.com>
2026-06-23 19:08:43 +0800

    Correct build issues caused by vcs_versioning==2.0 (#779)

--- src/rubicon/objc/__init__.py
+++ src/rubicon/objc/__init__.py
@@ -1,20 +1,4 @@
-try:
-    # Read version from SCM metadata
-    # This will only exist in a development environment
-    from setuptools_scm import get_version
-
-    # Excluded from coverage because a pure test environment (such as the one
-    # used by tox in CI) won't have setuptools_scm
-    __version__ = get_version("../../..", relative_to=__file__)  # pragma: no cover
-except (ModuleNotFoundError, LookupError):  # pragma: no-cover-if-missing-setuptools_scm
-    # If setuptools_scm isn't in the environment, the call to import will fail.
-    # If it *is* in the environment, but the code isn't a git checkout (e.g.,
-    # it's been pip installed non-editable) the call to get_version() will fail.
-    # If either of these occurs, read version from the installer metadata.
-
-    from importlib.metadata import version
-
-    __version__ = version("rubicon-objc")
+from importlib.metadata import version
 
 # `api`, `runtime` and `types` are only included for clarity. They are not
 # strictly necessary, because the from-imports below also import the types and
@@ -143,3 +127,5 @@ __all__ = [
     "runtime",
     "types",
 ]
+
+__version__ = version("rubicon-objc")
