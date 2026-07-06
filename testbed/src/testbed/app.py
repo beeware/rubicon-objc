8390feb11b0ff6ca5302245bfd5c47f8eb6d17c4
Russell Keith-Magee <russell@keith-magee.com>
2026-07-07 07:16:53 +0800

    Add initial testbed project.

---
+++ testbed/src/testbed/app.py
@@ -0,0 +1,8 @@
+"""A testbed for Rubicon ObjC.
+
+The testbed app has no logic of it's own - it can only be executed as a test suite.
+"""
+
+
+def main():
+    print("Did you forget to use `--test`?")
