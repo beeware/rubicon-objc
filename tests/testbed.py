a47f032b412cce032c81aa547448e932d1d0fa19
Russell Keith-Magee <russell@keith-magee.com>
2026-09-03 10:49:58 +0800

    Add a slowdown on test exit to ensure logs flush.

--- tests/testbed.py
+++ tests/testbed.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import tempfile
+import time
 from pathlib import Path
 
 import pytest
@@ -29,7 +30,12 @@ def run_tests():
         + args
     )
 
-    print(f">>>>>>>>>> EXIT {returncode} <<<<<<<<<<")
+    # Add a short pause to make sure any log tailing gets a chance to flush. Run a
+    # couple of times to make sure any log streaming dropouts don't prevent
+    # Briefcase from seeing the output.
+    for _ in range(6):
+        print(f">>>>>>>>>> EXIT {returncode} <<<<<<<<<<")
+        time.sleep(0.5)
 
 
 if __name__ == "__main__":
